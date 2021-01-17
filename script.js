52effe2deee19f4c07c1ba233bc37faa9f532d7e
SAYUK09 <sayurikamble123@gmail.com>
2021-01-17 20:49:04 +0530

    Logical Functionality Done

---
+++ script.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector(".btn");
+console.log(btn);
+const outputDiv = document.querySelector(".outputDiv");
+console.log(outputDiv);
+
+btn.addEventListener('click', clickHandler);
+
+function clickHandler(){
+   var input = document.querySelector(".inputDiv")
+   console.log(input.value)
+   var inputText = input.value
+
+    translate(inputText)
+}
+
+function translate(inputText){
+    var url ="https://api.funtranslations.com/translate/morse.json" 
+    var fetchUrl = `${url}?text=${inputText}`
+    console.log(fetchUrl)
+
+    fetch(fetchUrl)
+    .then(response=>response.json())
+    .then(json=>{
+        console.log(json)
+        var translatedText =json.contents.translated
+        outputDiv.innerHTML=translatedText
+        console.log(translatedText)
+    })
+}
